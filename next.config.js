d6939bb1edfc9823a28bf2f84a0308cca3e8d6cb
Tochilex <73448220+Tochilex@users.noreply.github.com>
2023-09-27 14:46:03 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
 
-    eslint: {
-        ignoreDuringBuilds: true,
-    }
-}
-=======
   eslint: {
         ignoreDuringBuilds: true,
     }
 }
 
->>>>>>> 059b925c3f21af44eaae59cd926226e7239071f9
 
 module.exports = nextConfig
